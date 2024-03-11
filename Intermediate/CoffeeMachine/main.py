8070f07f421c960eae1ed1db570ecdf124448e21
Nghia Nguyen <nghianguyen@Nghias-MBP.attlocal.net>
2024-03-10 19:04:46 -0500

    set up all dependencies and ready to start development

---
+++ Intermediate/CoffeeMachine/main.py
@@ -0,0 +1,14 @@
+# import all dependencies.
+from art import logo
+from data import coffee_machine_full_capacity
+from data import MENU
+import os
+
+# lambda to clear the console
+clear = lambda:os.system('clear')
+
+# need to be able to turn off the coffee machine, this will be taken into account when the user is prompt to enter a selection for the different type of coffee.
+
+
+# we need to prompt the user to see what coffee does the user wants. This will require us to give the different options and take in the input.
+
